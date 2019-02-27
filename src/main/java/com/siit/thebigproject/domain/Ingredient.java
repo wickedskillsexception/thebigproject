be501746d7152ec79875f6813077aacbd0edbc33
Teodora Budeanu <teo.budeanu@gmail.com>
2019-02-27 17:52:07 +0200

    Updated Ingredient

---
+++ src/main/java/com/siit/thebigproject/domain/Ingredient.java
@@ -0,0 +1,66 @@
+package com.siit.thebigproject.domain;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+public class Ingredient extends ObjectId{
+
+    @NotEmpty
+    @Size(max = 50)
+    private String name;
+
+    @NotEmpty
+    private MeasurementUnit unit;
+
+    public Ingredient(String name, String unit, int quantity) {
+        this.name = name;
+        this.unit = MeasurementUnit.valueOf(unit);
+
+    }
+
+    public Ingredient() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUnit() {
+        return unit.toString();
+    }
+
+
+    public void setUnit(String unit) {
+        this.unit = MeasurementUnit.valueOf(unit);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingredient that = (Ingredient) o;
+        return Objects.equals(name, that.name) &&
+                unit == that.unit;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, unit);
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "name='" + name + '\'' +
+                ", unit=" + unit +
+                '}';
+    }
+
+}
