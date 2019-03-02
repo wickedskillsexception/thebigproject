66f992650caa192a7af593ca53e43751066d5029
Teodora Budeanu <teo.budeanu@gmail.com>
2019-03-02 13:11:17 +0200

    Updated UserService

--- src/main/java/com/siit/thebigproject/service/UserService.java
+++ src/main/java/com/siit/thebigproject/service/UserService.java
@@ -1,13 +1,15 @@
 package com.siit.thebigproject.service;
 
-import com.siit.thebigproject.base.User;
-import com.siit.thebigproject.db.sql.IMUserDAO;
+import com.siit.thebigproject.db.DbException;
+import com.siit.thebigproject.domain.User;
+import com.siit.thebigproject.dao.sql.SQLUsersDAO;
 import com.siit.thebigproject.exceptions.ValidationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.Collection;
 
 
@@ -17,45 +19,44 @@ public class UserService {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
-    private IMUserDAO imUserDAO;
+    private SQLUsersDAO usersDAO;
 
-    public Collection <User> listAll() {
-        return imUserDAO.readAll();
+    public Collection <User> listAll() throws DbException, SQLException {
+        return usersDAO.getAll();
     }
 
 
-    public boolean delete(Long id) {
+    public boolean delete(Long id) throws DbException, SQLException {
         LOGGER.debug("Deleting templates for id: " + id);
-        User usr = imUserDAO.getById(id);
+        User usr = usersDAO.getById(id);
         if (usr != null) {
-            imUserDAO.delete(usr);
+            usersDAO.delete(usr);
             return true;
         }
 
         return false;
     }
 
-    public User get(Long id) {
+    public User get(Long id) throws DbException, SQLException {
 
         LOGGER.debug("Getting templates for id: " + id);
-        return imUserDAO.getById(id);
+        return usersDAO.getById(id);
 
     }
 
-    public void save(User user) throws ValidationException {
+    public void save(User user) throws ValidationException, DbException, SQLException {
         LOGGER.debug("Saving: " + user);
 //        validate(User);
 
-        imUserDAO.update(user);
+        usersDAO.update(user);
     }
 
-    public IMUserDAO getIMUserDAO() {
-
-        return imUserDAO;
+    public SQLUsersDAO getUsersDAO() {
+        return usersDAO;
     }
 
-    public void setIMUserDAO(IMUserDAO imUserDAO) {
-
-        this.imUserDAO = imUserDAO;
+    public void setUsersDAO(SQLUsersDAO usersDAO) {
+        this.usersDAO = usersDAO;
     }
 }
+
