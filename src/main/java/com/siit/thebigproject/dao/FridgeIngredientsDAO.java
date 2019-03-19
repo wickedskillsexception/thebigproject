b1952d71dfa469b34f8f94bfe73da1ef89e2015a
Teodora Budeanu <teo.budeanu@gmail.com>
2019-03-19 19:11:00 +0200

    Not commented

--- src/main/java/com/siit/thebigproject/dao/FridgeIngredientsDAO.java
+++ src/main/java/com/siit/thebigproject/dao/FridgeIngredientsDAO.java
@@ -1,13 +1,13 @@
-//package com.siit.thebigproject.dao;
-//
-//import com.siit.thebigproject.db.DbException;
-//import com.siit.thebigproject.domain.FridgeIngredient;
-//
-//import java.sql.SQLException;
-//import java.util.Collection;
-//
-//public interface FridgeIngredientsDAO extends BaseDAO<FridgeIngredient> {
-//
-//    public boolean deleteByFridgeId(long fridgeId) throws DbException, SQLException;
-//    public Collection<FridgeIngredient> getByFridgeId(long fridgeId) throws DbException, SQLException;
-//}
+package com.siit.thebigproject.dao;
+
+import com.siit.thebigproject.db.DbException;
+import com.siit.thebigproject.domain.FridgeIngredient;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+public interface FridgeIngredientsDAO extends BaseDAO<FridgeIngredient> {
+
+    public boolean deleteByFridgeId(long fridgeId) throws DbException, SQLException;
+    public Collection<FridgeIngredient> getByFridgeId(long fridgeId) throws DbException, SQLException;
+}
