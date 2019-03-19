5f0486fab5ac92b656fb212d663a1e9812433658
Teodora Budeanu <teo.budeanu@gmail.com>
2019-03-19 19:43:26 +0200

    Updated methods

--- src/main/java/com/siit/thebigproject/dao/UsersDAO.java
+++ src/main/java/com/siit/thebigproject/dao/UsersDAO.java
@@ -7,8 +7,6 @@ import java.sql.SQLException;
 
 public interface UsersDAO extends BaseDAO<User> {
 
-    public boolean updateUserDetails(String username, String email, int calories) throws DbException, SQLException;
-
     public boolean deleteByUsername(String username) throws DbException, SQLException;
 
 
