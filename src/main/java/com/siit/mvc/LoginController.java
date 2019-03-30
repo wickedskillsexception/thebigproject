b561b6dd448e0919f77fee40f10a6d773fea9d85
Bogdan <onicabogdan91@gmail.com>
2019-03-30 11:35:38 +0200

    Login Controller

---
+++ src/main/java/com/siit/mvc/LoginController.java
@@ -0,0 +1,24 @@
+package com.siit.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView("auth/login");
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/access-denied", method = RequestMethod.GET)
+    public ModelAndView accessDenied() {
+        ModelAndView modelAndView = new ModelAndView("auth/access-denied");
+
+        return modelAndView;
+    }
+}
