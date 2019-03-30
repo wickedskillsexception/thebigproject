a0ee15f03f288025dfab9964a021730ca4186373
Bogdan <onicabogdan91@gmail.com>
2019-03-30 11:44:23 +0200

    Password Encoding

---
+++ src/test/java/com/siit/thebigproject/service/PasswordEncodingTest.java
@@ -0,0 +1,15 @@
+package com.siit.thebigproject.service;
+
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncodingTest {
+
+    @Test
+    public void encriptPasswordWithBCrypt() {
+        String password = "user";
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String hashedPassword = encoder.encode(password);
+        System.out.println(hashedPassword);
+    }
+}
