879742815d907b386e6698944eb9b2ee13fdaa52
Bogdan <onicabogdan91@gmail.com>
2019-04-02 11:54:32 +0300

    Add role and user_role in user.sql

--- src/main/resources/user.sql
+++ src/main/resources/user.sql
@@ -4,7 +4,7 @@ CREATE SEQUENCE recipes_ids;
 CREATE TABLE recipes(id INT PRIMARY KEY DEFAULT NEXTVAL('recipes_ids'), name VARCHAR(32), preparation TEXT, preparation_time INTEGER, image VARCHAR(32), smart_points INTEGER);
 
 CREATE SEQUENCE users_ids;
-CREATE TABLE users(id INT PRIMARY KEY DEFAULT NEXTVAL('users_ids'), username VARCHAR(10), password VARCHAR(10), email VARCHAR(32));
+CREATE TABLE users(id INT PRIMARY KEY DEFAULT NEXTVAL('users_ids'), username VARCHAR(10), password VARCHAR(10), email VARCHAR(32), active boolean NOT NULL);
 
 CREATE SEQUENCE ingredients_ids;
 CREATE TABLE ingredients(id INT PRIMARY KEY DEFAULT NEXTVAL('ingredients_ids'), name VARCHAR(32), measurement_unit VARCHAR(32), unitFactorTransformation INTEGER);
@@ -18,6 +18,10 @@ CREATE TABLE recipe_ingredients(id INT PRIMARY KEY DEFAULT NEXTVAL('recipe_ingre
 CREATE SEQUENCE fridge_ingredients_ids;
 CREATE TABLE fridge_ingredients(id INT PRIMARY KEY DEFAULT NEXTVAL('fridge_ingredients_ids'), fridge_id INT REFERENCES fridges(id), name VARCHAR(32), measurement_unit VARCHAR(32), quantity DOUBLE PRECISION);
 
+CREATE SEQUENCE role_id_seq;
+CREATE TABLE role (id numeric NOT NULL DEFAULT nextval('role_id_seq'::regclass) PRIMARY KEY,name varchar(255) DEFAULT NULL);
+
+CREATE TABLE user_role (user_id numeric not null, role_id numeric not null, FOREIGN KEY ("user_id") REFERENCES app_user ("id"),FOREIGN KEY ("role_id") REFERENCES role ("id"));
 
 
 DROP DATABASE the_big_project;
